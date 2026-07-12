241d613cdfa07e6e128b7351a6f232d0f947ba38
Kanka Oza <kankaoza1609@gmail.com>
2026-07-12 18:31:43 +0530

    Revert "deployment done"
    This reverts commit f4fb3501622a622067c5938f7fd026868b47ccf9.

--- server/app.js
+++ server/app.js
@@ -15,10 +15,7 @@ import report_routes from "./routes/reportRoutes.js";
 const app=express();
 
 app.use(cors({
-    origin:[
-        "http://localhost:5173",
-        "https://transit-ops-mauve.vercel.app"
-    ],
+    origin:process.env.CLIENT_URL||"http://localhost:5173"||"https://transit-ops-mauve.vercel.app",
     credentials:true
 }));
 
