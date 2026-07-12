f4fb3501622a622067c5938f7fd026868b47ccf9
Kanka Oza <kankaoza1609@gmail.com>
2026-07-12 18:17:30 +0530

    deployment done

--- server/app.js
+++ server/app.js
@@ -15,7 +15,10 @@ import report_routes from "./routes/reportRoutes.js";
 const app=express();
 
 app.use(cors({
-    origin:process.env.CLIENT_URL||"http://localhost:5173"||"https://transit-ops-mauve.vercel.app",
+    origin:[
+        "http://localhost:5173",
+        "https://transit-ops-mauve.vercel.app"
+    ],
     credentials:true
 }));
 
